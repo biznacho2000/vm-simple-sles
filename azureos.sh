dc5f9cb0ba4259296ac87371a5687235f94d7cc5
shortkidd610 <shorty@shorty.net>
2018-02-12 10:59:11 -0800

    17th

--- azureos.sh
+++ azureos.sh
@@ -1,3 +1,7 @@
+#stop services
+rccron stop
+systemctl stop waagent
+
 #install hana prereqs
 sudo zypper install -y glibc-2.22-51.6
 sudo zypper install -y systemd-228-142.1
@@ -5,10 +9,7 @@ sudo zypper install -y unrar
 sudo zypper install -y sapconf
 sudo zypper install -y saptune
 sudo zypper se -t pattern
-sudo zypper in -t pattern sap-hana
-sudo saptune solution apply HANA
 sudo mkdir /etc/systemd/login.conf.d
-sudo mkdir /hana
 
 cp -f /etc/waagent.conf /etc/waagent.conf.orig
 sedcmd="s/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g"
@@ -19,24 +20,67 @@ cat /etc/waagent.conf | sed $sedcmd | sed $sedcmd2 > /etc/waagent.conf.new
 cp -f /etc/waagent.conf.new /etc/waagent.conf
 
 
+mv /var /var.new
+mv /home /home.new
+mv /tmp /tmp.new
+mv /opt /opt.new
+
+mv 
+
 echo "logicalvols start" >> /tmp/parameter.txt
   vg_infraagentlun="$(lsscsi $number 0 0 3 | grep -o '.\{9\}$')"
   pvcreate $vg_infraagentlun
+  pvcreate $vg_system
   vgcreate vg_infraagent $vg_infraagentlun 
-  lvcreate -l 100%FREE -n lv_infraagent vg_infraagent
-  mkfs.xfs /dev/hanavg/lv_infraagentlun
+  vgcreate vg_system $vg_system
+  lvcreate -L 4G -n lv_var vg_system
+  lvcreate -L 4G -n lv_home vg_system
+  lvcreate -L 8G -n lv_tmp vg_system
+  lvcreate -L 4G -n lv_opt vg_system
+mkfs.ext4 /dev/vg_system/lv_var
+mkfs.ext4 /dev/vg_system/lv_home
+mkfs.ext4 /dev/vg_system/lv_tmp
+mkfs.ext4 /dev/vg_system/lv_opt
+
 echo "logicalvols end" >> /tmp/parameter.txt
+
 
 #!/bin/bash
 echo "mounthanashared start" >> /tmp/parameter.txt
-mount -t xfs /dev/sharedvg/sharedlv /hana/shared
+
+mkdir /var
+mkdir /home
+mkdir /tmp
+mkdir /opt
+
+mount -t ext4 /dev/vg_system/lv_var /var
+mount -t ext4 /dev/vg_system/lv_home /home
+mount -t ext4 /dev/vg_system/lv_tmp /tmp
+mount -t ext4 /dev/vg_system/lv_opt /opt
+
 mkdir /hana/data/sapbits
 echo "mounthanashared end" >> /tmp/parameter.txt
 
+#data moves
+mv /var.new/* /var
+mv /var.new/.* /var
+mv /home.new/* /home
+mv /home.new.* /home
+mv /tmp.new/* /tmp
+mv /tmp.new/.* /tmp
+mv /opt.new/* /opt
+mv /opt.new/.* /opt
+
 echo "write to fstab start" >> /tmp/parameter.txt
-echo "/dev/mapper/hanavg-datalv /hana/data xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/usrsapvg-usrsaplv /usr/sap xfs defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/vg_system-lv_home /home ext4 defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/vg_system-lv_tmp /tmp ext4 defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/vg_system-lv_opt /opt ext4 defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/vg_system-lv_var /var ext4 defaults 0 0" >> /etc/fstab
 echo "write to fstab end" >> /tmp/parameter.txt
 
 
 echo "update boot.ini"
+
+
+echo "final reboot to reenable boot.ini and services"
+shutdown -r now
