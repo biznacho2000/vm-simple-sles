89492ddd203f9d7356edafa1c3917e213d3a5781
shortkidd610 <shorty@shorty.net>
2018-02-14 08:59:04 -0800

    32

--- azureos_min.sh
+++ azureos_min.sh
@@ -1,2 +1,12 @@
-touch /tmp/vincent
-touch /tmp/vincent_completed
+#stop services
+rccron stop
+systemctl stop waagent
+
+#install hana prereqs
+zypper install -y glibc-2.22-51.6
+zypper install -y systemd-228-142.1
+zypper install -y unrar
+zypper install -y sapconf
+zypper install -y saptune
+zypper se -t pattern
+mkdir /etc/systemd/login.conf.d
