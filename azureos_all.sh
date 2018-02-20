ccaa4cb44aadf2e262c402bc0f87fc3c29a035cf
Christopher Nolan <christopher.nolan@microsoft.com>
2018-02-20 15:23:46 -0800

    changing shutdown to 1 minute rather than now and fixing boot.local permissions

--- azureos_all.sh
+++ azureos_all.sh
@@ -7,7 +7,7 @@ sudo zypper install -y saptune
 sudo zypper se -t pattern
 sudo mkdir /etc/systemd/login.conf.d
 
-#update audit 
+#update audit
 
 echo "write to audit config begin" >> /tmp/audit.txt
 echo "install cramfs /bin/true" >> /etc/modprobe.d/CIS.conf
@@ -21,7 +21,7 @@ echo "install vfat /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install dccp /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install sctp /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "options ipv6 disable=1" >> /etc/modprobe.d/CIS.conf
-systemctl disable autofs     
+systemctl disable autofs
 echo "write to audit config end" >> /tmp/audit.txt
 
 echo "update boot.ini"
@@ -30,7 +30,7 @@ mv /etc/rc.d/boot.local /etc/rc.d/boot.local.orig
 cd /tmp
 /usr/bin/wget --quiet "https://raw.githubusercontent.com/shortkidd610/vm-simple-sles/master/boot.sh"
 echo "sh /tmp/boot.sh >> /tmp/boot.sh.log 2>&1" > /etc/rc.d/boot.local
-chmod 755 /etc/rc.d/boot.local
+chmod 744 /etc/rc.d/boot.local
 
 echo "final reboot to reenable boot.ini and services"
-shutdown -r now
+shutdown -r 1
